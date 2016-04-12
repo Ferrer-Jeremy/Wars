5f17e09cec0b6facb45c34cb0d20e47dd80ee1b9
jack-hyde <jackhyde3@gmail.com>
2016-04-12 14:21:51 +0200

    Attack zone fini

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -71,8 +71,6 @@ public class Controls : MonoBehaviour
         int rangeMax;
         int departX;
         int departY;
-        int xBis;
-        int yBis;
         int[,] zone = new int[100,2];   //ATTENTION !!!!!!!!!!!!!!    LIMTE LA TAILLE DE LA ZONE A 100 CASES
         int i = 0;
 
@@ -96,7 +94,6 @@ public class Controls : MonoBehaviour
                     {
                         for (int yPlus = 0; yPlus <= rangeMin - xPlus; yPlus++)
                         {
-
                             zone[i, 0] = xPlus + departX;
                             zone[i, 1] = yPlus + departY;
                             i++;
@@ -108,15 +105,15 @@ public class Controls : MonoBehaviour
                             i++;
                         }
                     }
-                    for (int xMoins = 0; xMoins <= rangeMin; xMoins++)
+                    for (int xMoins = 0; xMoins >= -rangeMin; xMoins--)
                     {
-                        for (int yPlus = 0; yPlus <= rangeMin - xMoins; yPlus++)
+                        for (int yPlus = 0; yPlus <= rangeMin + xMoins; yPlus++)
                         {
                             zone[i, 0] = xMoins + departX;
                             zone[i, 1] = yPlus + departY;
                             i++;
                         }
-                        for (int yMoins = 0; yMoins >= -rangeMin + xMoins; yMoins--)
+                        for (int yMoins = 0; yMoins >= -rangeMin - xMoins; yMoins--)
                         {
                             zone[i, 0] = xMoins + departX;
                             zone[i, 1] = yMoins + departY;
@@ -131,18 +128,15 @@ public class Controls : MonoBehaviour
                         {
                             for (int k = 0; k < zone.GetLength(0); k++)
                             {
-                                if (zone[k, 0] != 0 || zone[k, 1] != 0)// si l'un des 2 est different de 0
+                                if (zone[j, 0] == zone[k, 0] && zone[j, 1] == zone[k, 1] && k != j) //si on a 2 fois le meme chiffre (sauf 0,0) on met le 2eme a zero
                                 {
-                                    if (zone[j, 0] == zone[k, 0] && zone[j, 1] == zone[k, 1]) //si on a 2 fois le meme chiffre (sauf 0,0) on met le 2eme a zero
-                                    {
-                                        zone[k, 0] = 0;
-                                        zone[k, 1] = 0;
-                                    }
+                                    zone[k, 0] = 0;
+                                    zone[k, 1] = 0;                                 
                                 }
                             }
                         }
                     }
-
+                    
                     for (int j = 0; j < zone.GetLength(0); j++)    //test pour afficher les coordonnées differentes de 0,0
                     {
                         if (zone[j, 0] != 0 || zone[j, 1] != 0)
