ed07a97a2d3c1ebfb9d7efe93432c6da6dad045f
jack <jackhyde3@gmail.com>
2016-06-14 08:27:27 +0200

    Design du menu complet, fonctionalité a finir

---
+++ Assets/Scripts/CreationMap/MapJson.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+[Serializable]
+public class MapJson : MonoBehaviour
+{
+    private string nom;
+    private int largeur;
+    private int longeur;
+    private string[,] cases;
+    private string path = @"c:\temp\";
+
+    public MapJson (string nom, int largeur, int longeur, string[,] cases)
+    {
+        this.nom = nom;
+        this.largeur = largeur;
+        this.longeur = longeur;
+        this.cases = cases;
+    }
+
+    public void enregistrer(MapJson mapJson)
+    {
+        string json = JsonUtility.ToJson(mapJson);
+        using (StreamWriter sw = File.CreateText(path+nom+".json"))
+        {
+            sw.WriteLine(json);
+        }
+    }
+
+    /*MapJson charger()
+    {
+       // return JsonUtility.FromJson<MyClass>();
+    }*/
+}
--- Assets/Scripts/CreationMap/UI.cs
+++ Assets/Scripts/CreationMap/UI.cs
@@ -1,32 +1,138 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
 
-    public bool positionListeCases;
+    public GameObject terrain;
+
+    public GameObject menu;
+    public GameObject quitter;
+    public GameObject save;
+    public GameObject load;
+    public Text nomMap;
+    public Text largeur;
+    public Text longeur;
+    private string map;
+    private string largeurMap;
+    private string longeurMap;
+
+    //pour recuperer la taille de la carte
+    public GameObject gameManager;
+    private CreationMap creationMap; // le script
+    private int largeurSave;
+    private int longeurSave;
+    private int debutLargeur;
+    private int debutLongeur;
 
 
-	// Use this for initialization
-	void Start () {
-        positionListeCases = true;
-
+    // Use this for initialization
+    void Start () {
+        //pour recuperer la taille de la carte
+        creationMap = gameManager.GetComponent<CreationMap>();
+        largeurSave = creationMap.getLargeur();
+        longeurSave = creationMap.getLongeur();
+        debutLargeur = creationMap.getDebutLargeur();
+        debutLongeur = creationMap.getDebutLongeur();
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-    public void positionPanel(bool panelHaut)
-    {
-        Debug.Log("Button was pressed");
-        if (panelHaut)
+        if(Input.GetButtonDown("Cancel"))// fait apparaitre ou disparaitre la menu
         {
-            positionListeCases = false;
+            if(menu.activeSelf)
+            {
+                menu.SetActive(false);
+                quitter.SetActive(false);
+                save.SetActive(false);
+                load.SetActive(false);
+            }
+            else
+            {
+                menu.SetActive(true);
+            }
+        }
+    }
+
+    public void quitterButton() //pour le bouton quitter et quitter non
+    {
+        if (quitter.activeSelf)
+        {
+            quitter.SetActive(false);
         }
         else
         {
-            positionListeCases = true;
+            quitter.SetActive(true);
         }
     }
+
+    public void quitterButtonOui() //pour le bouton quitter oui
+    {
+
+    }
+
+    public void newMap() //pour le bouton nouvelle carte
+    {
+        // recupere les inputs largeur et longeur et recharge la scene avec ces parametres si il n'y en a pas met des valeur pas defaut
+    }
+
+    public void saveMap() //fait apparaitre et disparaitre la fenetre enregistrer a l'aide du bouton enregistrer et enregistrer non
+    {
+        if (save.activeSelf)
+        {
+            save.SetActive(false);
+        }
+        else
+        {
+            save.SetActive(true);
+        }
+    }
+
+    public void saveMapOui()// pour le bouton sauvegarder oui
+    {
+        //recupere le champ input pour enregistrer la carte si il n'y en a pas l'enregistre sous un nom generique
+        string[,] cases = new string[largeurSave + debutLargeur + 1, longeurSave + debutLongeur + 1];
+        Transform[] allChildrenTerrain;
+        allChildrenTerrain = terrain.GetComponentsInChildren<Transform>();
+
+        for(int x = debutLargeur; x <= largeurSave + debutLargeur; x++)
+        {
+            for(int y = debutLongeur; x <= longeurSave + debutLongeur; y++)
+            {
+                foreach(Transform boop in allChildrenTerrain)
+                {
+                    if(boop.position.x == x && boop.position.y == y)
+                    {
+                        cases[x, y] = boop.tag; //a changer pour recuperer le bon type de case exactement
+                    }
+                }
+            }
+        }
+
+
+        MapJson mapJson = new MapJson(nomMap.text, largeurSave, longeurSave, cases);
+
+        //nomMap.text
+        Debug.Log(nomMap.text);
+    }
+
+    public void loadMap()//fait apparaitre et disparaitre la fenetre charger a l'aide du bouton charger et charger non
+    {
+        if (load.activeSelf)
+        {
+            load.SetActive(false);
+        }
+        else
+        {
+            load.SetActive(true);
+            // affiche toutes les maps presente dans le dossier assets/maps
+        }
+    }
+
+    public void loadMapOui()//pour le bouton valider dans charger 
+    {
+        //relance la scene avec cette carte
+    }
+
 }
